380374863c03a3a8424b20ff0f6ef68dd3ed8d60
konglong <1016361268@qq.com>
2023-02-04 18:44:48 +0800

    头像退出

---
+++ src/layout/headers/components/avatar.vue
@@ -0,0 +1,30 @@
+<template>
+  <el-dropdown>
+    <span class="el-dropdown-link">
+      <el-avatar shape="square" :size="40" :src="squareUrl"></el-avatar>
+    </span>
+    <template #dropdown>
+      <el-dropdown-menu>
+        <el-dropdown-item @click="logout">退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </template>
+  </el-dropdown>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useStore } from 'vuex'
+const store = useStore()
+const squareUrl = ref(
+  'https://img2.baidu.com/it/u=2421090168,324781765&fm=253&app=138&size=w931&n=0&f=JPEG&fmt=auto?sec=1675616400&t=82b58c6ffc7f3cd2b6f38288e8b02d50'
+)
+const logout = () => {
+  store.dispatch('app/logout')
+}
+</script>
+
+<style lang="scss" scoped>
+::v-deep .el-dropdown-menu__item {
+  white-space: nowrap;
+}
+</style>
